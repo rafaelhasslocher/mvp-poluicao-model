a2a9fd27ebf8f8535f83da8bbc97d480c07f7f96
Rafael Hasslocher <rafaelhasslocher@yahoo.com>
2024-12-08 12:15:00 -0300

    fix: ajuste nos hiperparâmetros para tentar melhorar o ajuste. acerto do inverse_transform do minmaxscaler()

--- ajuste_lstm.py
+++ ajuste_lstm.py
@@ -68,29 +68,29 @@ for group in groups:
     i += 1
 plt.show()
 
-sns.set_theme(style="darkgrid")
+# sns.set_theme(style="darkgrid")
 
-fig, axs = plt.subplots(3, 2, figsize=(24, 14))
+# fig, axs = plt.subplots(3, 2, figsize=(24, 14))
 
-sns.histplot(
-    data=df_test_scaled, x="residuals", kde=True, color="skyblue", ax=axs[0, 0]
-)
-sns.histplot(data=df_test_scaled, x="dew", kde=True, color="olive", ax=axs[0, 1])
-sns.histplot(data=df_test_scaled, x="temp", kde=True, color="gold", ax=axs[1, 0])
-sns.histplot(data=df_test_scaled, x="press", kde=True, color="teal", ax=axs[1, 1])
-sns.histplot(
-    data=df_test_scaled, x="wnd_dir", kde=True, color="steelblue", ax=axs[2, 0]
-)
-sns.histplot(
-    data=df_test_scaled, x="wnd_spd", kde=True, color="goldenrod", ax=axs[2, 1]
-)
+# sns.histplot(
+#     data=df_test_scaled, x="residuals", kde=True, color="skyblue", ax=axs[0, 0]
+# )
+# sns.histplot(data=df_test_scaled, x="dew", kde=True, color="olive", ax=axs[0, 1])
+# sns.histplot(data=df_test_scaled, x="temp", kde=True, color="gold", ax=axs[1, 0])
+# sns.histplot(data=df_test_scaled, x="press", kde=True, color="teal", ax=axs[1, 1])
+# sns.histplot(
+#     data=df_test_scaled, x="wnd_dir", kde=True, color="steelblue", ax=axs[2, 0]
+# )
+# sns.histplot(
+#     data=df_test_scaled, x="wnd_spd", kde=True, color="goldenrod", ax=axs[2, 1]
+# )
 
-plt.show()
+# plt.show()
 
 scaler = MinMaxScaler()
 
 
-columns = ["dew", "temp", "press", "wnd_dir", "wnd_spd", "snow", "rain", "residuals"]
+columns = ["residuals", "dew", "temp", "press", "wnd_dir", "wnd_spd", "snow", "rain"]
 
 
 df_train_scaled[columns] = scaler.fit_transform(df_train_scaled[columns])
@@ -130,17 +130,17 @@ model = Sequential()
 model.add(
     LSTM(64, input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=True)
 )
-model.add(Dropout(0.2))
+model.add(Dropout(0.01))
 model.add(LSTM(16, return_sequences=False))
 model.add(Dense(y_train.shape[1]))
 
 
 model.compile(
-    loss="mse", optimizer=Adam(learning_rate=0.001), metrics=[RootMeanSquaredError()]
+    loss="mse", optimizer=Adam(learning_rate=0.00001), metrics=[RootMeanSquaredError()]
 )
 
 early_stopping = EarlyStopping(
-    monitor="val_loss", patience=10, restore_best_weights=True
+    monitor="val_loss", patience=20, restore_best_weights=True
 )
 checkpoint = ModelCheckpoint(
     "best_model.keras", monitor="val_loss", save_best_only=True
@@ -151,8 +151,8 @@ model.summary()
 history = model.fit(
     X_train,
     y_train,
-    epochs=20,
-    batch_size=32,
+    epochs=40,
+    batch_size=64,
     validation_split=0.1,
     callbacks=[early_stopping, checkpoint],
     shuffle=False,
@@ -160,38 +160,35 @@ history = model.fit(
 
 best_model = load_model("best_model.keras")
 
-plt.figure(figsize=(15, 6))
-plt.plot(history.history["loss"], label="Training Loss")
-plt.plot(history.history["val_loss"], label="Validation Loss")
-plt.title("Training and Validation Loss")
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
-plt.legend()
-plt.show()
+# plt.figure(figsize=(15, 6))
+# plt.plot(history.history["loss"], label="Training Loss")
+# plt.plot(history.history["val_loss"], label="Validation Loss")
+# plt.title("Training and Validation Loss")
+# plt.xlabel("Epochs")
+# plt.ylabel("Loss")
+# plt.legend()
+# plt.show()
 
-test_predictions = best_model.predict(X_test).flatten()
+test_predictions = best_model.predict(X_test)
 
-test_predictions_reshaped = test_predictions.reshape(-1, 1)
+vitor = np.repeat(test_predictions, 8, axis=1)
 
-zeros = np.zeros((test_predictions_reshaped.shape[0], df_test_scaled.shape[1] - 1))
+vitor = scaler.inverse_transform(vitor)
 
-test_predictions_full = np.hstack([zeros, test_predictions_reshaped])
-test_predictions_original = scaler.inverse_transform(test_predictions_full)[:, -1]
+vitor = vitor[:, 0]
 
-y_test_reshaped = y_test.flatten().reshape(-1, 1)
-zeros_y_test = np.zeros((y_test_reshaped.shape[0], df_test_scaled.shape[1] - 1))
-y_test_full = np.hstack([zeros_y_test, y_test_reshaped])
-y_test_original = scaler.inverse_transform(y_test_full)[:, -1]
+y_test_vitor = np.repeat(y_test, 8, axis=1)
 
-test_results = pd.DataFrame(
-    data={"Train Predictions": test_predictions_original}
-)
+y_test_vitor = scaler.inverse_transform(y_test_vitor)
+
+y_test_vitor = y_test_vitor[:, 0]
+
+test_results = pd.DataFrame(data={'Train Predictions': vitor,
+                                  'Actual':y_test_vitor})
 test_results.head()
 
-plt.plot(test_results["Train Predictions"][:200], label="Predicted Values")
+plt.plot(test_results['Train Predictions'][:200], label='Predicted Values')
+plt.plot(test_results['Actual'][:200], label='True Values')
 plt.legend()
-plt.show()
-
-plt.plot(df_test["residuals"][:200], label="Values")
-plt.legend()
-plt.show()
+rmse = sqrt(mse(y_test, test_predictions))
+print('Test RMSE: %.5f' % rmse)
